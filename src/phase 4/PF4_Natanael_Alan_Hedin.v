ab4a5b3325aa5dfe027c954abe8c06d0b198a2d7
Alan Reyes <alan.reyes@upr.edu>
2023-04-14 18:17:17 -0400

    Updating el file con mas modulos existentes

--- src/phase 4/PF4_Natanael_Alan_Hedin.v
+++ src/phase 4/PF4_Natanael_Alan_Hedin.v
@@ -211,6 +211,7 @@ end
 
 endmodule
 
+//Partes de la implementacion de la fase 2
 /************************************************************************************************************************************************************************************************************************************************************************/
 
 // Control Unit module
@@ -355,13 +356,7 @@ module control_unit(output reg I31, I30, I24, I13,
     end
 endmodule
 
-// Instruction Memory module
-module ROM (input [8:0] address, output reg [31:0] DataOut);
-    reg [7:0] mem [0:511]; // 512x8 ROM
-    always @(address) begin
-        DataOut = {mem[address], mem[address+1], mem[address+2], mem[address+3]};
-    end
-endmodule
+
 
 // Multiplexer module for Control Unit
 module ctrl_unit_mux_2x1(output reg I31_OUT, I30_OUT, I24_OUT, I13_OUT, ID_Load_Instr_OUT, ID_RF_Enable_OUT,
@@ -620,6 +615,197 @@ module Pipeline_Register_MEM_WB(output reg [31:0] PW_WB,
     end
 endmodule
 
+
+
+//Partes del Register File Implementation
+/************************************************************************************************************************************************************************************************************************************************************************/
+
+module Binary_Decoder ( //El módulo tiene tres puertos: D, E y O. D es una entrada de 5 bits, E es una entrada de 1 bit y O es una salida de 32 bits.
+    input [4:0] D,
+    input E,
+    output reg [31:0] O
+);
+
+always @(*) begin
+    if (E == 1'b1) begin
+        case (D)
+            5'b00000: O = 32'b00000000000000000000000000000001;
+            5'b00001: O = 32'b00000000000000000000000000000010;
+            5'b00010: O = 32'b00000000000000000000000000000100;
+            5'b00011: O = 32'b00000000000000000000000000001000;
+            5'b00100: O = 32'b00000000000000000000000000010000;
+            5'b00101: O = 32'b00000000000000000000000000100000;
+            5'b00110: O = 32'b00000000000000000000000001000000;
+            5'b00111: O = 32'b00000000000000000000000010000000;
+            5'b01000: O = 32'b00000000000000000000000100000000;
+            5'b01001: O = 32'b00000000000000000000001000000000;
+            5'b01010: O = 32'b00000000000000000000010000000000;
+            5'b01011: O = 32'b00000000000000000000100000000000;
+            5'b01100: O = 32'b00000000000000000001000000000000;
+            5'b01101: O = 32'b00000000000000000010000000000000;
+            5'b01110: O = 32'b00000000000000000100000000000000;
+            5'b01111: O = 32'b00000000000000001000000000000000;
+            5'b10000: O = 32'b00000000000000010000000000000000;
+            5'b10001: O = 32'b00000000000000100000000000000000;
+            5'b10010: O = 32'b00000000000001000000000000000000;
+            5'b10011: O = 32'b00000000000010000000000000000000;
+            5'b10100: O = 32'b00000000000100000000000000000000;
+            5'b10101: O = 32'b00000000001000000000000000000000;
+            5'b10110: O = 32'b00000000010000000000000000000000;
+            5'b10111: O = 32'b00000000100000000000000000000000;
+            5'b11000: O = 32'b00000001000000000000000000000000;
+            5'b11001: O = 32'b00000010000000000000000000000000;
+            5'b11010: O = 32'b00000100000000000000000000000000;
+            5'b11011: O = 32'b00001000000000000000000000000000;
+            5'b11100: O = 32'b00010000000000000000000000000000;
+            5'b11101: O = 32'b00100000000000000000000000000000;
+            5'b11110: O = 32'b01000000000000000000000000000000;
+            5'b11111: O = 32'b10000000000000000000000000000000;
+    endcase
+    end
+    else 
+        O = 32'b00000000000000000000000000000000;
+end
+endmodule
+
+/************************************************************************************************************************************************************************************************************************************************************************/
+
+//Registers:
+// Descripcion:el módulo Register se encarga de cargar los datos de entrada Ds en el registro Q en la posición indicada 
+// por la señal Ld cuando ésta es diferente de 0, y de proporcionar como salida Qs los bits [31:1] del registro Q. 
+// La señal Clk es la señal de reloj que se utiliza para sincronizar el registro.
+
+module Register (
+    input [31:0] Ds, // Entrada de datos que se cargarán en el registro.
+    output reg [31:0] Qs, // Salida del registro.
+    input Ld, // Señal que indica cuándo cargar los datos en el registro. //LE en el documento
+    input Clk // Señal de reloj.
+);
+
+always@(posedge Clk) begin
+    if(Ld) Qs <=Ds;
+end
+
+endmodule
+
+/************************************************************************************************************************************************************************************************************************************************************************/
+
+//Multiplexors:
+// Descripcion: El módulo Multiplexer_32x1 toma dos entradas de 32 bits llamadas S y D, y produce una salida de 32 bits llamada Y.
+// La entrada S especifica qué uno de los 32 bits en D se debe seleccionar y enviar a la salida Y.
+
+module Multiplexer_32x1 (
+input [4:0] S, // Entrada selectora de 5 bits
+  input [31:0] D0, D1, D2, D3, D4, D5, D6, D7, D8, D9, D10, D11, D12, D13, D14, D15, D16, D17, D18, D19, D20, D21, D22, D23, D24, D25, D26, D27, D28, D29, D30, D31, // Entrada de datos de 32 bits
+output reg [31:0] Y // Salida de datos de 32 bits
+);
+
+always @ (*)
+begin
+case(S)
+5'b00000: Y = D0;
+5'b00001: Y = D1;
+5'b00010: Y = D2;
+5'b00011: Y = D3;
+5'b00100: Y = D4;
+5'b00101: Y = D5;
+5'b00110: Y = D6;
+5'b00111: Y = D7;
+5'b01000: Y = D8;
+5'b01001: Y = D9;
+5'b01010: Y = D10;
+5'b01011: Y = D11;
+5'b01100: Y = D12;
+5'b01101: Y = D13;
+5'b01110: Y = D14;
+5'b01111: Y = D15;
+5'b10000: Y = D16;
+5'b10001: Y = D17;
+5'b10010: Y = D18;
+5'b10011: Y = D19;
+5'b10100: Y = D20;
+5'b10101: Y = D21;
+5'b10110: Y = D22;
+5'b10111: Y = D23;
+5'b11000: Y = D24;
+5'b11001: Y = D25;
+5'b11010: Y = D26;
+5'b11011: Y = D27;
+5'b11100: Y = D28;
+5'b11101: Y = D29;
+5'b11110: Y = D30;
+5'b11111: Y = D31;
+endcase
+end
+endmodule
+
+
+//Partes del RAM/ROM Implementation
+/************************************************************************************************************************************************************************************************************************************************************************/
+
+// Instruction Memory module
+module ROM (input [8:0] address, output reg [31:0] DataOut);
+    reg [7:0] mem [0:511]; // 512x8 ROM
+    always @(address) begin
+        DataOut = {mem[address], mem[address+1], mem[address+2], mem[address+3]};
+    end
+endmodule
+
+module RAM(output reg[31:0] DataOut, input RW, input[8:0] address, input[31:0] DataIn, input [1:0] Size, input SE, input E);//inicacion de variables
+
+  reg[7:0] mem[0:511]; // RAM 512x8
+  reg [31:0] temp;
+  
+  always @ (RW, address, DataIn, Size, SE, E)       
+    
+        case(Size)
+        2'b00://localizaciones de byte para leer y escribir 
+          if (RW==1 && E ==1) //When Write 
+            begin
+              mem[address] = DataIn[7:0]; 
+            end
+          else if (RW==0 && E==1 && SE == 0)
+            begin
+                DataOut = {24'b000000000000000000000000, mem[address]};
+            end  
+          else if (RW==0 && E==1 && SE ==1)begin
+            temp = {24'b000000000000000000000000, mem[address]};
+            if (temp[7]==1) DataOut = {24'b111111111111111111111111, mem[address]};
+            else DataOut = temp;
+          end 
+        2'b01: //localizaciones de halfword para leer y escribir
+            if (RW==1 && E ==1) //When Write 
+            begin
+              mem[address] = DataIn[15:8];
+              mem[address+1] = DataIn[7:0]; 
+            end
+          else if (RW==0 && E==1 && SE == 0)
+            begin
+              DataOut = {16'b0000000000000000, mem[address], mem[address+1]};
+            end  
+          else if (RW==0 && E==1 && SE ==1)begin
+            temp = {16'b0000000000000000, mem[address], mem[address+1]};
+            if (temp[15]==1) DataOut = {16'b1111111111111111, mem[address], mem[address+1]};
+            else DataOut = temp;
+          end 
+        2'b10: //localizaciones de word para leer y escribir
+          if (RW == 1 && E== 1) //When Write 
+            begin
+                mem[address] = DataIn[31:24];
+                mem[address + 1] = DataIn[23:16];
+                mem[address + 2] = DataIn[15:8]; 
+                mem[address + 3] = DataIn[7:0]; 
+            end                 
+            else //When Read
+            begin
+                DataOut = ({mem[address + 0], mem[address + 1], mem[address + 2], mem[address + 3]}); // 4 espacios en memoria
+            end    
+    endcase  
+endmodule
+
+//Partes del ALU/Source Operan2 Handler Implementation
+/************************************************************************************************************************************************************************************************************************************************************************/
+
 // ALU module
 module alu_sparc_component (output reg [31:0] Out, output reg Z, N, C, V, 
                             input [31:0] A, B, input [3:0] OP, input Cin);
@@ -653,3 +839,32 @@ module alu_sparc_component (output reg [31:0] Out, output reg Z, N, C, V,
         end
     end
 endmodule
+
+//Source Operand2 Handler
+module source_operand2_handler_sparc_component(output reg [31:0] N, 
+                                                input [31:0] R, 
+                                                input [21:0] Imm, 
+                                                input [3:0] Is);
+    always @(R, Imm, Is)
+        case(Is)
+            4'b0000: N = {Imm, 10'b0000000000};
+            4'b0001: N = {Imm, 10'b0000000000};
+            4'b0010: N = {Imm, 10'b0000000000};
+            4'b0011: N = {Imm, 10'b0000000000};
+            4'b0100: N = {{10{Imm[21]}}, Imm}; // For sign extended replicate MSB
+            4'b0101: N = {{10{Imm[21]}}, Imm};
+            4'b0110: N = {{10{Imm[21]}}, Imm};
+            4'b0111: N = {{10{Imm[21]}}, Imm};
+            4'b1000: N = R;
+            4'b1001: N = {{19{Imm[12]}}, Imm[12:0]};
+            4'b1010: N = {{27{1'b0}}, R[4:0]};
+            4'b1011: N = {{27{1'b0}}, Imm[4:0]};
+            4'b1100: N = R;
+            4'b1101: N = {{19{Imm[12]}}, Imm[12:0]};
+            4'b1110: N = R;
+            4'b1111: N = {{19{Imm[12]}}, Imm[12:0]};
+        endcase
+endmodule
+
+
+/************************************************************************************************************************************************************************************************************************************************************************/
